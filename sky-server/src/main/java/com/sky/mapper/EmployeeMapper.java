1bf75cf5ec702aab3a4c463452e37129354fd766
陈慧炫 <16035206+Camelia1375@user.noreply.gitee.com>
2026-03-18 20:49:31 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -20,8 +20,8 @@ public interface EmployeeMapper {
      * 插入员工数据
      * @param employee
      */
-    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
-            "values "+
-            "(#{name},#{username},#{phone},#{sex},#{idNumber},#{creatTime},#{updateTime},#{createUser},#{updateUser},#{status})")
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status) " +
+            "values " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status})")
     void insert(Employee employee);
 }
