a7c07a193818919bd89525af2d34204443973ec9
陈慧炫 <16035206+Camelia1375@user.noreply.gitee.com>
2026-03-23 21:16:57 +0800

    商品浏览和套餐增删改业务开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -60,15 +60,11 @@ public interface DishMapper {
     @AutoFill(value = OperationType.UPDATE)
     void update(Dish dish);
 
-    /**
-     * 动态条件查询菜品数据
-     */
-    List<Dish> list(Dish dish);
 
     /**
      * 根据套餐id查询菜品选项
      */
-    @Select("select  a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id ={setmealId}")
+    @Select("select  a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id =#{setmealId}")
     List<Dish> getBysetmealId(Long setmealId);
 
     /**
@@ -81,4 +77,19 @@ public interface DishMapper {
      * @param ids
      */
     void deleteByIds(List<Long> ids);
+
+    /**
+     * 动态条件查询菜品
+     * @param dish
+     * @return
+     */
+    List<Dish> list(Dish dish);
+
+    /**
+     * 根据套餐id查询菜品
+     * @param setmealId
+     * @return
+     */
+    @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
+    List<Dish> getBySetmealId(Long setmealId);
 }
