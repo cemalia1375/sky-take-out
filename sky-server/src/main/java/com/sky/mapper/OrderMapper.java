8608f32309f6b5015e2fcc83e87149d2f5fb2e12
陈慧炫 <16035206+Camelia1375@user.noreply.gitee.com>
2026-03-24 23:01:48 +0800

    订单支付功能业务开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -14,4 +14,18 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
 }
